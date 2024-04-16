c84cd7c8336ffae0b844864b43acdb3900e3b1f3
Aditya Patel <adityapatel21052022@gmail.com>
2024-04-16 14:13:29 +0530

    trying to add jstl tag but failed

--- springmvc/src/main/java/springmvc/controller/HomeController.java
+++ springmvc/src/main/java/springmvc/controller/HomeController.java
@@ -1,14 +1,21 @@
 package springmvc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("this is home url");
+		model.addAttribute("name","Alpha");
+		model.addAttribute("rollno",101);
 		return "index";
 	}
 	@RequestMapping("/about")
@@ -16,5 +23,17 @@ public class HomeController {
 		System.out.println("Guten Tag");
 		System.out.println("Wie geht es dir!");
 		return "about";
+	}
+	@RequestMapping("/help")
+	public ModelAndView help() {
+		System.out.println("this is hlep");
+		ModelAndView mv=new ModelAndView();
+		List<String>  frind=new ArrayList<>();
+		frind.add("alpha");
+		frind.add("beta");
+		frind.add("gamma");
+		mv.addObject("name",frind);
+		mv.setViewName("help");
+		return mv;
 	}
 }
--- springmvc/src/main/webapp/WEB-INF/views/index.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -9,6 +9,16 @@
 <body>
 	<h1>This is Home page</h1>
 	<h2>Called By home controller</h2>
-	<h1>Url is /hom</h1>
+	<h1>Url is /home</h1>
+	<%
+	String name=(String)request.getAttribute("name");
+	int rollno=(Integer)request.getAttribute("rollno");
+	%>
+	<h1>My name is 
+	<%=name %>
+	</h1>
+	<h1>My rollno is 
+	<%=rollno%>
+	</h1>
 </body>
 </html>
